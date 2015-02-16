9bc1917f56bb557da436ea46e0edfe973075dee4
SGcom <quisa137@gmail.com>
2015-02-16 20:28:40 +0900

    oc ios library 서브모듈로 만들고 변경 사항 업데이트 함

---
+++ NCDrive/SystemConstants.h
@@ -0,0 +1,13 @@
+//
+//  SystemConstants.h
+//  NCDrive
+//
+//  Created by 박신구 on 2015. 2. 16..
+//  Copyright (c) 2015년 Jindata. All rights reserved.
+//
+
+#ifndef NCDrive_SystemConstants_h
+#define NCDrive_SystemConstants_h
+
+
+#endif
